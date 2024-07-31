41c7227c648e046ec509e1013a69f0c056e66eda
AristideBH <aristide.bruneau@gmail.com>
2024-07-31 20:29:19 +0200

    up

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -8,7 +8,8 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({ page: 'build' }),
+		adapter: adapter(),
+		outDir: 'build',
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
