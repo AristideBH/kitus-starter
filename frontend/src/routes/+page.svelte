249db0c4f9d8d428d88dcce39bba8c33a2cb67e8
AristideBH <aristide.bruneau@gmail.com>
2024-08-16 12:53:41 +0200

    update

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,24 +5,24 @@
 </script>
 
 <section class="grid gap-6 py-8">
-	<h2>Welcome to Starterkit</h2>
+	<h1>Welcome to Starterkit</h1>
 
-	<p>A custom boilerplate for Sveltekit and Directus</p>
+	<p class="lead">A custom boilerplate for Sveltekit and Directus</p>
 	<summary class="bg-muted list-none rounded-sm p-4">
-		<details open>
+		<details open={true}>
 			<ul class="mt-4">
-				<li>Sveltekit</li>
-				<li>Directus</li>
-				<li>Types sync</li>
-				<li>Authentification</li>
-				<li>Tailwind and Shadcn-svelte</li>
+				<li>Svelte 5 & Sveltekit</li>
+				<li>Directus SDK and decicated functions</li>
+				<li>Client Types & Schema sync</li>
+				<li>Authentification & account page</li>
+				<li>Tailwind and shadcn-svelte</li>
 				<li>Pages transitions</li>
 				<li>
-					Components
+					Components :
 					<ul>
 						<li>Image</li>
 						<li>Pagination</li>
-						<li>Load preprocessor</li>
+						<li>Flexible Builder</li>
 					</ul>
 				</li>
 			</ul>
