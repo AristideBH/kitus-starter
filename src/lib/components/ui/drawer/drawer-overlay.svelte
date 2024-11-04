fee1efc591058e2d071f85c71ae71ac2569c566b
AristideBH <aristide.bruneau@gmail.com>
2024-11-04 11:29:31 +0100

    ui: update components

--- src/lib/components/ui/drawer/drawer-overlay.svelte
+++ src/lib/components/ui/drawer/drawer-overlay.svelte
@@ -1,25 +1,16 @@
 <script lang="ts">
-	import { Drawer as DrawerPrimitive } from 'vaul-svelte';
-	import { cn } from '$lib/logic/utils.js';
-	import { fade } from 'svelte/transition';
+	import { Drawer as DrawerPrimitive } from "vaul-svelte";
+	import { cn } from "$lib/logic/utils.js";
 
-	type $$Props = DrawerPrimitive.OverlayProps;
-
-	export let el: $$Props['el'] = undefined;
-	let className: $$Props['class'] = undefined;
-	export let transition: $$Props['transition'] = fade;
-	export let transitionConfig: $$Props['transitionConfig'] = {
-		duration: 150
-	};
-	export { className as class };
+	let {
+		ref = $bindable(null),
+		class: className,
+		...restProps
+	}: DrawerPrimitive.OverlayProps = $props();
 </script>
 
 <DrawerPrimitive.Overlay
-	bind:el
-	{transition}
-	{transitionConfig}
-	class={cn('fixed inset-0 z-50 bg-black/80', className)}
-	{...$$restProps}
->
-	<slot />
-</DrawerPrimitive.Overlay>
+	bind:ref
+	class={cn("fixed inset-0 z-50 bg-black/80", className)}
+	{...restProps}
+/>
